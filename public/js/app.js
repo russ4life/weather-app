ffed876a4287b0eb031a9aa6d7f5385f1277635d
Russell Burdikin <russell.burdikin@yahoo.com>
2019-07-14 20:00:04 +0100

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -6,8 +6,10 @@ const messageTwo = document.querySelector('#message-2')
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const location = search.value
+
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
+
     fetch('/weather?address=' + location).then((response) => {
 
         response.json().then(({ error, address, location, forecast} = {}) => {
